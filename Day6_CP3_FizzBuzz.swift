3f382c2d17e5e6e9ae62c0a29b1a38e027ff5313
GwenUI <85646150+GwenUI@users.noreply.github.com>
2022-01-08 18:11:17 +0100

    Create Day6_CP3_FizzBuzz.swift

---
+++ Day6_CP3_FizzBuzz.swift
@@ -0,0 +1,14 @@
+let fizz = 3
+let buzz = 5
+
+for i in 1...100 {
+    if i.isMultiple(of: fizz) && i.isMultiple(of: buzz) {
+        print("FizzBuzz")
+    } else if i.isMultiple(of: fizz) {
+        print("Fizz")
+    } else if i.isMultiple(of: buzz) {
+        print("Buzz")
+    } else {
+        print(i)
+    }
+}
